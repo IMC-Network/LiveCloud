1f28c47e69d1169253231f62d387bf4ac45cbb86
James-Livesey <jamesliveg@gmail.com>
2019-02-07 13:02:29 +0000

    Add app tiles

--- apps.js
+++ apps.js
@@ -2,10 +2,25 @@ var currentUser = {
     uid: null
 };
 
+function addApp() {
+    $("<div class='appTile'>").appendTo($(".appsList"));
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         currentUser.uid = user.uid;
     } else {
         window.location.href = "index.html";
     }
+});
+
+$(function() {
+    addApp();
+    addApp();
+    addApp();
+    addApp();
+    addApp();
+    addApp();
+    addApp();
+    addApp();
 });
