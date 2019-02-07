03340cf170cb9e2175a3bdecbdae6323ced8d0f9
James-Livesey <jamesliveg@gmail.com>
2019-02-07 10:43:19 +0000

    Add disabled button style

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function signIn() {
+    $(".signInButton").attr("disabled", "");
+    $(".signInButton").text("Signing In...");
+
     firebase.auth().signInWithEmailAndPassword($("#email").val(), $("#password").val()).catch(function(error) {
         $("#signInError").text(error.message);
+
+        $(".signInButton").removeAttr("disabled");
+        $(".signInButton").text("Sign In");
     });
 }
 
