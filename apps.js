402ee5c245886c630198f27429c30b39c846c2b1
James-Livesey <jamesliveg@gmail.com>
2019-02-08 08:26:14 +0000

    Add sign-in system

--- apps.js
+++ apps.js
@@ -9,6 +9,30 @@ function addApp() {
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         currentUser.uid = user.uid;
+
+        firebase.database().ref("users/" + currentUser.uid + "/version").once("value", function(versionSnapshot) {
+            firebase.database().ref("users/" + currentUser.uid + "/versionID").once("value", function(versionIDSnapshot) {
+                if (versionSnapshot.val() != null) {
+                    if (versionSnapshot.val() != LiveCloud.version) {
+                        dialog("LiveCloud has been updated!", `
+                            <strong>Welcome to LiveCloud version ` + LiveCloud.version + `!</strong>
+                            ` + LiveCloud.changelog + `
+                        `);
+                    }
+                } else {
+                    dialog("Welcome to LiveCloud!", `
+                        <strong>Welcome to IMC LiveCloud!</strong> We're glad to
+                        have you on-board. If you ever need help, just read the
+                        documentation or get live support by pressing the Help
+                        button in the top bar. We can't wait to see what you
+                        create!
+                    `);
+                }
+
+                firebase.database().ref("users/" + currentUser.uid + "/version").set(LiveCloud.version);
+                firebase.database().ref("users/" + currentUser.uid + "/versionID").set(LiveCloud.versionID);
+            });
+        });
     } else {
         window.location.href = "index.html";
     }
---
+++ script.js
@@ -0,0 +1,63 @@
+var LiveCloud = {
+    version: "0.1.0",
+    versionID: 0,
+    changelog: `IMC has been developing this update to bring new features to the table, including:
+        <ul>
+            <li>More LiveCloud stuff!</li>
+        </ul>
+    `
+};
+
+function dialog(title, content, buttons = [{text: "Close", onclick: "closeDialog();", type: "primary"}], allowEscape = true) {
+    $(".dialog").html(`
+        <div class="dialogTitle"></div>
+        <div class="dialogContent"></div>
+        <div class="dialogActions"></div>
+    `);
+
+    $(".dialogTitle").text(title);
+    $(".dialogContent").html(content);
+
+    for (var i = 0; i < buttons.length; i++) {
+        $(".dialogActions").html(
+            $(".dialogActions").html() +
+            "<button" +
+            (
+                buttons[i].type == "secondary" ?
+                " class='secondary'" :
+                (
+                    buttons[i].type == "bad" ?
+                    " class='bad'" :
+                    (
+                        buttons[i].type == "reallyBad" ?
+                        " class='reallyBad'" :
+                        (
+                            buttons[i].type == "highlight" ?
+                            " class='highlight'" :
+                            ""
+                        )
+                    )
+                )
+            ) +
+            " onclick='" +
+            buttons[i].onclick +
+            "'>" +
+            buttons[i].text +
+            "</button>"
+        );
+    }
+
+    if (allowEscape) {
+        $(".dialogBackground").attr("onclick", "closeDialog();");
+    } else {
+        $(".dialogBackground").attr("onclick", "");
+    }
+
+    $(".dialogBackground").fadeIn();
+    $(".dialog").fadeIn();
+}
+
+function closeDialog() {
+    $(".dialogBackground").fadeOut();
+    $(".dialog").fadeOut();
+}
